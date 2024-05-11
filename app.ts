20c175f771712e3532126bb072b656444d779769
VANDEIR DE SOUZA CRUZ <84112426+vandeir95@users.noreply.github.com>
2024-05-10 22:27:41 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,34 @@
+
+import { CompanyAccount } from './class/CompanyAccount'
+import { PeopleAccount } from './class/PeopleAccount'
+import { SpecialPersonAccount } from './class/SpecialPersonAccount'
+
+
+
+
+
+
+
+   
+
+    //const peopleAccount: PeopleAccount = new PeopleAccount(1,'vandeir', 10, 0)
+  //  console.log(peopleAccount)
+    // peopleAccount.deposit(60)
+   //  peopleAccount.withdraw(50)
+   
+   // const companyAccount: CompanyAccount = new CompanyAccount('BH', 12, 0, )
+   // companyAccount.deposit(500)
+    //companyAccount.withdraw(100)
+    //companyAccount.getLoan(1000)
+
+    const  specialPersonAccount = new SpecialPersonAccount(1,'vandeir', 10, 0)
+
+    specialPersonAccount.deposit(60)
+
+
+    //console.log(companyAccount) 
+
+
+
+
+    
